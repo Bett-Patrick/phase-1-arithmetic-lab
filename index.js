d952a066a25f98ca1abe8e777c6b25ac532f64f3
Bett <bettpatrick080@gmail.com>
2023-12-16 17:47:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+//multiply
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+//random
+const random = Math.floor(Math.random()*10)+1
+
+//modulus/division
+const num3 = 29
+const num4 = 5
+const mod = num3%num4
+
+//Math.max
+const max = Math.max(1,13,5,4,20)
+
